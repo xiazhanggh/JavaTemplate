613b76b1298f34af4db61af3c3eceb374fe3efb3
xzhanggh <10860248@qq.com>
2017-07-18 04:19:45 -0700

    socketio

--- src/main/java/com/build/oa/socketio/MsgPushServer.java
+++ src/main/java/com/build/oa/socketio/MsgPushServer.java
@@ -3,6 +3,7 @@ package com.build.oa.socketio;
 import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.Transport;
 import com.corundumstudio.socketio.listener.DataListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -25,16 +26,22 @@ public class MsgPushServer {
 
     @Bean(destroyMethod = "stop")
     public SocketIOServer startServer(){
-        com.corundumstudio.socketio.Configuration configuration = new com.corundumstudio.socketio.Configuration();
-        configuration.setHostname(hostName);
-        configuration.setPort(port);
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname(hostName);
+        config.setPort(port);
+        config.setPingTimeout(60000); //60s server will close client connection (default)
+        config.setPingInterval(25000); // 25s server will emit a new ping packet (default)
+        config.setTransports(new Transport[]{Transport.WEBSOCKET,Transport.POLLING});//use websocket > polling
+        //config.setOrigin("localhost:80");
 
-        final SocketIOServer server = new SocketIOServer(configuration);
+
+        final SocketIOServer server = new SocketIOServer(config);
+
 
         server.addEventListener("chatevent",EmitObject.class,new DataListener<EmitObject>(){
             @Override
             public void onData(SocketIOClient client, EmitObject data, AckRequest ackSender) throws Exception {
-                 server.getBroadcastOperations().sendEvent("chatevent",data);
+                 server.getBroadcastOperations().sendEvent("chatevent",data);//Broadcast
             }
         });
         server.start();
